691b38d54c59b817d2ff6bebd53b739fc4e2860e
Administración <rudy@lacabinagris.com>
2021-04-12 20:49:50 +0200

    Intial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+import turtle
+
+class Circuito():
+    Corredores =[]
+    
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        
+        self.screen = turtle.Screen()
+        
+        
