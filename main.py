aa65c442b6beee49dca7e50dca7c71014daf10ff
rudyhostyn <68102881+rudyhostyn@users.noreply.github.com>
2021-04-14 10:13:43 +0200

    Circuito creado

--- main.py
+++ main.py
@@ -7,6 +7,14 @@ class Circuito():
         self.width = width
         self.height = height
         
-        self.screen = turtle.Screen()
+        self.__screen = turtle.Screen()
+        self.__screen.setup(width, height)
+        self.__screen.bgcolor("lightgray")
+        
+        for i in Range (4):
+            new_turtle = turtle.Turtle()
+            self.Corredores.append(new_turtle)
         
         
+if __name__ == "__main__":
+    circuito = Circuito(640, 480)
