728e2b20cb0f0caf55155673321f838e4b67a629
rudyhostyn <68102881+rudyhostyn@users.noreply.github.com>
2021-04-14 10:44:07 +0200

    Turtugas creadas

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import turtle
 
 class Circuito():
     Corredores =[]
+    TurtleColor = ("red","blue","green","purple")
+    __posStartY = (-60, -20, 20, 60)
     
     def __init__(self, width, height):
         self.width = width
@@ -10,9 +12,16 @@ class Circuito():
         self.__screen = turtle.Screen()
         self.__screen.setup(width, height)
         self.__screen.bgcolor("lightgray")
+        self.__startline = -width/2+20
+        self.__finishtline = width/2-20
         
-        for i in Range (4):
+        for i in range (4):
             new_turtle = turtle.Turtle()
+            new_turtle.penup()
+            new_turtle.shape("turtle")
+            new_turtle.color(self.TurtleColor[i])
+            new_turtle.setpos(self.__startline, self.__posStartY[i])
+            
             self.Corredores.append(new_turtle)
         
         
